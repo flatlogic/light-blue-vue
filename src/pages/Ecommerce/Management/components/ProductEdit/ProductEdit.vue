454f059867cab8d4d3f053d8e103853924d7d57c
shoudaos <shoudaos92@gmail.com>
2019-07-04 09:44:22 +0300

    fix(e-commerce): fixed title displaying, fixed default image

--- src/pages/Ecommerce/Management/components/ProductEdit/ProductEdit.vue
+++ src/pages/Ecommerce/Management/components/ProductEdit/ProductEdit.vue
@@ -9,7 +9,7 @@
           <div class="col-md-5">
             <b-dropdown variant="info" id="productImage">
               <template slot="button-content">
-                <img class="product-image" alt="img" :src="product.img"/>
+                <img v-if="product.img" class="product-image" alt="img" :src="product.img"/>
               </template>
               <b-dropdown-item v-for="img in images" :key="img" @click="updateProductProperty(img, 'img')">
                 <img class="product-image" :alt="img" :src="img"/>
@@ -123,7 +123,6 @@
   import InputTag from 'vue-input-tag'
   import Widget from '@/components/Widget/Widget';
   import Loader from '@/components/Loader/Loader';
-  import image from '@/assets/products/img1.jpg';
 
   export default {
     name: "ProductEdit",
@@ -134,22 +133,22 @@
       }
     },
     mounted() {
-      this.getProductsImagesRequest();
+      let newProduct = {
+        id: -1,
+        price: '0.01',
+        rating: '5',
+        technology: []
+      };
       let product = this.findProduct(this.getId());
       if (this.getId() > -1) {
         this.loadProductRequest(this.getId());
       } else {
         if (!product) {
-          this.receiveProduct({
-            id: -1,
-            img: image,
-            price: '0.01',
-            rating: '5',
-            technology: []
-          });
+          this.receiveProduct(newProduct);
         }
 
       }
+      this.getProductsImagesRequest(newProduct);
     },
     computed: {
       ...mapState("products", ["products", "isReceiving", "isDeleting", "isUpdating", "images"]),
