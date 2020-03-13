8ac5ba083f19510eed72c16154e16058619c620f
shoudaos <shoudaos92@gmail.com>
2020-03-13 13:05:51 +0300

    updated seed

---
+++ src/pages/Another/Another.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard-page">
+    <h1 class="page-title">Another <small>Page</small></h1>
+  </div>
+</template>
+
+<script>
+import Widget from '@/components/Widget/Widget';
+
+export default {
+  name: 'Another',
+  components: {
+    Widget
+  }
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,6 @@ import Vuex from 'vuex';
 import layout from './layout';
 import auth from './auth';
 import register from './register';
-import dashboard from './dashboard';
-import products from './products';
 
 Vue.use(Vuex);
 
@@ -14,7 +12,5 @@ export default new Vuex.Store({
     layout,
     auth,
     register,
-    dashboard,
-    products,
   },
 });
