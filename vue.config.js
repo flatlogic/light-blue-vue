870863f8f4610bb425be18d4cdf29b58659125ad
Blari <Blarior@gmail.com>
2020-12-03 15:44:32 +0300

    - Fix dist folder - Fix several form elements

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-let publicPath = process.env.NODE_ENV !== 'production' ? '/' : 'light-blue-vue/';
+const path = require("path");
+let publicPath = process.env.NODE_ENV !== "production" ? "/" : "./";
 
 module.exports = {
   publicPath,
   productionSourceMap: false,
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
       const terserWebpackPlugin = config.optimization.minimizer[0];
       const terserOptions = terserWebpackPlugin.options.terserOptions;
       terserOptions.mangle = {
-        reserved: ['$super']
+        reserved: ["$super"],
       };
     }
     config.resolve.alias["jquery"] = path.join(__dirname, "./jqueryStub.js");
-  }
+  },
 };
